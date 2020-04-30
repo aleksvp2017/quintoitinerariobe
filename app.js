157d0d9ea435e118db87c2f4d105dc5973af81c0
Aleksander Pascoal <aleksvp@MacBook-Air-de-Aleksander.local>
2020-04-29 21:10:36 -0300

    Corrige endpoint bemvindo

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/alterarSenha', Usuario.alterarSenha)
 
 //WELCOME
 app.get('/bemvindo', (request, response, next) => {
-    response.status(200).json({ mensagem: res.rows[0] })
+    response.status(200).json({ mensagem: "Seja bem vindo" })
 } )
 
 
